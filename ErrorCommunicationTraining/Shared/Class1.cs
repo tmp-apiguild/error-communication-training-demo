76767a1068e0accc172da1c7126361e3329ddfae
Stian Trondsen <stian.trondsen@arvato.com>
2022-08-23 16:17:03 +0200

    Add project files.

---
+++ ErrorCommunicationTraining/Shared/Class1.cs
@@ -0,0 +1,9 @@
+﻿namespace Shared
+{
+    public class Error
+    {
+        public string Category { get; set; }
+        public string Type { get; set; }
+        public string Title { get; set; }
+    }
+}
