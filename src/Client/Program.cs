af34c26b566d15a8027552f8ebea6c93f162b3cc
Stian Trondsen <stian.trondsen@arvato.com>
2022-08-24 16:22:53 +0200

    mudblazor

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -1,13 +1,15 @@
 using Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-var uri = builder.Configuration["api"];
+var uri = builder.Configuration["api"] ?? throw new Exception("Api url missing :(");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(uri) });
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
