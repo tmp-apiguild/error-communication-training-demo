203db57171cb6a5f46f318e99ad98bd85ea516b1
Stian Trondsen <stian.trondsen@arvato.com>
2022-08-25 14:42:01 +0200

    code example

---
+++ src/Client/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using MudBlazor;
+
+namespace Client;
+
+public static class Extensions
+{
+    public static void AddError<T>(this MudTextField<T> input, string error)
+    {
+        input.ErrorText = error;
+        input.Error = true;
+    }
+}
